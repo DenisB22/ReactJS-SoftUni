ceb825659c4bcdcae8375faf91014b35596d16ea
DenisB22 <bechiragich97@gmail.com>
2023-02-23 15:54:41 +0200

    first-react-app

---
+++ 01.Intro to React and JSX/first-react-app/src/App.js
@@ -0,0 +1,21 @@
+import Logo from './components/Logo'
+import './App.css';
+import Paragraph from './components/Paragraph';
+import Link from './components/Link';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo />
+        <Paragraph />
+        <Paragraph />
+        <Paragraph />
+        <Paragraph />
+        <Link />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01.Intro to React and JSX/first-react-app/src/components/Paragraph.js
@@ -0,0 +1,7 @@
+const Paragraph = () => {
+    return <p> 
+    Some text 2
+  </p>;
+}
+
+export default Paragraph;
