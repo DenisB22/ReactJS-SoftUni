979ace89f8d154e6e8f6a5dba61d6be9df695396
DenisB22 <bechiragich97@gmail.com>
2023-03-03 10:22:41 +0200

    Todo List extended

--- 03.Components - Deep Dive/todo-list - practice/client/src/App.js
+++ 03.Components - Deep Dive/todo-list - practice/client/src/App.js
@@ -1,21 +1,19 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Loading from "./components/Loading";
 import TodoList from "./components/TodoList";
 
-
 function App() {
   const [todos, setTodos] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-    
 
   useEffect(() => {
-    fetch(`http://localhost:3030/jsonstore/todos`)
-      .then((res) => res.json())
+    fetch('http://localhost:3030/jsonstore/todos')
+      .then(res => res.json())
       .then((data) => {
-        const result = Object.keys(data).map(id => ({id, ...data[id]}))
+        const result = Object.keys(data).map(id => ({id, ...data[id]}));
         setTodos(result);
         console.log(data);
         setIsLoading(false);
@@ -25,7 +23,7 @@ function App() {
   const onTodoAdd = () => {
     const lastId = Number(todos[todos.length - 1].id);
     const text = prompt('Task name:');
-    const newTask = { id: lastId + 1, text, isCompleted: false };  
+    const newTask = { id: lastId + 1, text, isCompleted: false };
 
     setTodos(state => [newTask, ...state]);
   };
@@ -48,8 +46,8 @@ function App() {
 
           <div className="table-wrapper">
             {isLoading
-                ? <Loading />
-                : <TodoList todos={todos}  toggleTodoStatus={toggleTodoStatus} />
+              ? <Loading />
+              : <TodoList todos={todos} toggleTodoStatus={toggleTodoStatus} />
             }
           </div>
         </section>
