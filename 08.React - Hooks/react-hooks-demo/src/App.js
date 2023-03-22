8655df34daa2e20a6f2437f1d6d8ab3493104ca2
DenisB22 <bechiragich97@gmail.com>
2023-03-22 17:09:46 +0200

    React Hooks

---
+++ 08.React - Hooks/react-hooks-demo/src/App.js
@@ -0,0 +1,96 @@
+import { useState, useEffect } from "react";
+
+import { TodoContext } from "./contexts/TodoContext";
+import { Header } from "./components/Header";
+import { TodoList } from "./components/TodoList";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AddTodoModal } from "./components/AddTodoModal";
+
+const baseUrl = "http://localhost:3030/jsonstore/todos";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [showAddTodo, setShowAddTodo] = useState(false);
+
+  useEffect(() => {
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then((result) => {
+        setTodos(Object.values(result));
+
+        // setTodos(state => {
+        //   // [...state, 'asdsfafa'];
+        //   // state.slice();
+        //   // Object.assign({}, state);
+
+        //   return state;
+        // });
+
+        // Don't do the following!!!
+        // setTodos(state => {
+        //   state.push({});
+
+        //   return state;
+        // });
+      });
+  }, []);
+
+  const onTodoAddSubmit = async (values) => {
+    // console.log(values);
+    const response = await fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        "content-type": "applicaiton/json",
+      },
+      body: JSON.stringify(values),
+    });
+
+    const result = await response.json();
+
+    setShowAddTodo(false);
+    setTodos((state) => [...state, result]);
+
+    console.log(result);
+  };
+
+  const onTodoAddClick = () => {
+    setShowAddTodo(true);
+  };
+
+  const onTodoAddClose = () => {
+    setShowAddTodo(false);
+  };
+
+  const onTodoDeleteClick = async (todoId) => {
+    await fetch(`${baseUrl}/${todoId}`, { method: "DELETE" });
+
+    setTodos((state) => state.filter((x) => x._id !== todoId));
+  };
+
+  const contextValue = {
+    onTodoDeleteClick
+  };
+
+  return (
+    <TodoContext.Provider value={contextValue}>
+      <div>
+        <Header />
+
+        <TodoList
+          todos={todos}
+          onTodoAddClick={onTodoAddClick}
+          
+        />
+
+        <AddTodoModal
+          show={showAddTodo}
+          onTodoAddSubmit={onTodoAddSubmit}
+          onTodoAddClose={onTodoAddClose}
+        />
+      </div>
+    </TodoContext.Provider>
+  );
+}
+
+export default App;
---
+++ 08.React - Hooks/react-hooks-demo/src/components/TodoItem.js
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import { ListGroup, Button } from "react-bootstrap";
+
+import { TodoContext } from '../contexts/TodoContext';
+
+export const TodoItem = ({
+    _id,
+    text,
+    isCompleted,
+}) => {
+    const { onTodoDeleteClick } = useContext(TodoContext);
+
+    return (
+        <ListGroup.Item action style={{ display: 'flex', justifyContent: 'space-between' }}>
+            {text}
+            <Button variant="dark" onClick={() => onTodoDeleteClick(_id)}>X</Button>
+        </ListGroup.Item>
+    );
+};
