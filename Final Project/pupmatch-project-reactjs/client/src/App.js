41318662214966dfca44b813c2f200d2b89cdc91
DenisB22 <bechiragich97@gmail.com>
2023-04-10 18:09:24 +0300

    Pupmatch Blog Template Fixed

--- Final Project/pupmatch-project-reactjs/client/src/App.js
+++ Final Project/pupmatch-project-reactjs/client/src/App.js
@@ -1,57 +1,52 @@
-import { useState, useEffect, useContext } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-
+import { useState, useEffect, useContext } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
-import { ChatBox } from './components/Chat/ChatBox';
-import { Details } from './components/Details/Details';
-import { EditProfile } from './components/EditProfile/EditProfile';
+import { ChatBox } from "./components/Chat/ChatBox";
+import { Details } from "./components/Details/Details";
+import { EditProfile } from "./components/EditProfile/EditProfile";
+import Blog from './components/Blog/Blog';
 
+import * as dogService from "./services/dogService";
+import { AuthContext, AuthContextProvider } from "./context/AuthContext";
 
-import * as dogService from './services/dogService';
-import { AuthContext, AuthContextProvider } from './context/AuthContext';
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
-import { db } from './firebase';
-import { collection,  getDocs } from 'firebase/firestore';
-
-import './components/Chat/style.scss';
-
+import "./components/Chat/style.scss";
 
 function App() {
   const [cards, setCards] = useState([]); // Storing the data when we fetch all the docs
   const [card, setCard] = useState({});
   const [clearChat, setClearChat] = useState(false);
 
-
   const usersCollectionRef = collection(db, "users");
 
-  const {currentUser} = useContext(AuthContext);
- 
+  const { currentUser } = useContext(AuthContext);
+
   // console.log(currentUser);
 
-
-  const ProtectedRoute = ({children}) => {
-    if(!currentUser) {
-      return <Navigate to="/" />
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/" />;
     }
 
     return children;
   };
 
-  const getCardFromDetails = (dataFromDetails) =>  {
+  const getCardFromDetails = (dataFromDetails) => {
     // console.log('Hello from getCardFromDetails')
     setCard(dataFromDetails);
     // console.log('hello')
     // console.log(`card from App: ${dataFromDetails}`);
     setClearChat(false);
-  }
+  };
 
   function updateClearChatState(value) {
     setClearChat(value);
   }
-
 
   useEffect(() => {
     // console.log('Hello from useEffect');
@@ -59,44 +54,50 @@ function App() {
       // console.log("getUsers called");
       const data = await getDocs(usersCollectionRef);
       // console.log(data);
-      setCards(data.docs.map(doc => ({...doc.data(), id: doc.id})));
-    }
+      setCards(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
     // console.log(cards.length)
     getUsers();
   }, []);
 
-
-
   return (
-    
     <div>
       <Routes>
-        <Route path="/" element={<Home cards={cards} currentUser={currentUser} />} /> 
+        <Route
+          path="/"
+          element={<Home cards={cards} currentUser={currentUser} />}
+        />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+
         <Route path="/register" element={<Register setCards={setCards} />} />
-        {/* <Route path="/register" element={<Register />} /> */}
-        <Route path="/messages" element={
-        <ProtectedRoute>
-          <ChatBox card={card} clearChat={clearChat} updateClearChatState={updateClearChatState} />
-        </ProtectedRoute>
-        } />
-        {/* <Route path="/messages" element={<ChatBox />} /> */}
-        {/* <Route path='/details' element={<Details card={card} />} /> */}
-        {/* <Route path='/details' element={<Details/>} /> */}
-        
 
-          <Route path='/details/:uid' element={<Details setCards={setCards} sendData={getCardFromDetails}/>}/>
+        <Route path="/blog" element={<Blog />} />
 
-        {/* <Route path='edit/:uid' element={<EditProfile />} /> */}
-        <Route path='edit/:uid' element={<EditProfile setCards={setCards} />} />
-        
+        <Route
+          path="/messages"
+          element={
+            <ProtectedRoute>
+              <ChatBox
+                card={card}
+                clearChat={clearChat}
+                updateClearChatState={updateClearChatState}
+              />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/details/:uid"
+          element={
+            <Details setCards={setCards} sendData={getCardFromDetails} />
+          }
+        />
+
+        <Route path="edit/:uid" element={<EditProfile setCards={setCards} />} />
+       
       </Routes>
     </div>
   );
 }
 
-
-
-
-export default (App);
+export default App;
