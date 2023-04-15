c59015567c1a75be631929ba4573cb342209fb44
DenisB22 <bechiragich97@gmail.com>
2023-04-15 15:24:54 +0300

    Pupmatch Fixed Register Data Validation

--- Final Project/pupmatch-project-reactjs/client/src/components/Register/Register.js
+++ Final Project/pupmatch-project-reactjs/client/src/components/Register/Register.js
@@ -35,18 +35,40 @@ export const Register = ({ setCards }) => {
   // const [err, setErr] = useState(false);
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
+  const [formValid, setFormValid] = useState(false);
 
   const [firstNameError, setFirstNameError] = useState("");
+  const [isFirstNameValid, setIsFirstNameValid] = useState(true);
+
   const [lastNameError, setLastNameError] = useState("");
+  const [isLastNameValid, setIsLastNameValid] = useState(true);
+
   const [cityError, setCityError] = useState("");
+  const [isCityValid, setIsCityValid] = useState(true);
+
   const [countryError, setCountryError] = useState("");
+  const [isCountryValid, setIsCountryValid] = useState(true);
+
   const [genderError, setGenderError] = useState("");
+  const [isGenderValid, setIsGenderValid] = useState(true);
+
   const [ageError, setAgeError] = useState("");
+  const [isAgeValid, setIsAgeValid] = useState(true);
+
   const [breedError, setBreedError] = useState("");
+  const [isBreedValid, setIsBreedValid] = useState(true);
+
   const [imageError, setImageError] = useState("");
+  const [isImageValid, setIsImageValid] = useState(true);
+
   const [emailError, setEmailError] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(true);
+
   const [passwordError, setPasswordError] = useState("");
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+
   const [additionalInfoError, setAdditionalInfoError] = useState("");
+  const [isAdditionalInfoValid, setIsAdditionalInfoValid] = useState(true);
 
   // const validateEmail = async (email) => {
   //   const usersRef = collection(db, "users");
@@ -60,6 +82,216 @@ export const Register = ({ setCards }) => {
   //   } else {
   //     setEmailError("");
   //     setEmail(email);
+  //   }
+  // };
+
+  const handleFirstNameChange = (event) => {
+    const firstName = event.target.value;
+    if (!firstName) {
+      setFirstNameError("Please enter first name!");
+      setIsFirstNameValid(false);
+    } else if (firstName.length < 2 || firstName.length > 50) {
+      setFirstNameError("First name must be between 2 and 50 characters!");
+      setIsFirstNameValid(false);
+    } else if (!/^[A-Za-z]+$/.test(firstName)) {
+      setFirstNameError("Please enter a valid first name!");
+      setIsFirstNameValid(false);
+    } else {
+      setFirstNameError("");
+      setIsFirstNameValid(true);
+    }
+  };
+
+  const handleLastNameChange = (event) => {
+    const lastNameValue = event.target.value.trim();
+    if (!lastNameValue) {
+      setLastNameError("Please enter last name!");
+      setIsLastNameValid(false);
+    } else if (lastNameValue.length < 2 || lastNameValue.length > 50) {
+      setLastNameError("Last name must be between 2 and 50 characters!");
+      setIsLastNameValid(false);
+    } else if (!/^[A-Za-z]+$/.test(lastNameValue)) {
+      setLastNameError("Please enter a valid last name!");
+      setIsLastNameValid(false);
+    } else {
+      setLastNameError("");
+      setIsLastNameValid(true);
+    }
+  };
+  
+
+  const handlePasswordChange = (event) => {
+    const { value } = event.target;
+    if (!value) {
+      setPasswordError("Please enter a password!");
+      setIsPasswordValid(false);
+    } else if (value.length < 8 || value.length > 32) {
+      setPasswordError("Password must be between 8 and 32 characters!");
+      setIsPasswordValid(false);
+    } else if (!/\d/.test(value) || !/[a-zA-Z]/.test(value)) {
+      setPasswordError(
+        "Password must contain at least one letter and one number!"
+      );
+      setIsPasswordValid(false);
+    } else {
+      setPasswordError("");
+      setIsPasswordValid(true);
+    }
+  };
+
+  const handleCityChange = (event) => {
+    const cityValue = event.target.value.trim();
+    if (!cityValue) {
+      setCityError("Please enter city name!");
+      setIsCityValid(false);
+    } else if (cityValue.length < 2 || cityValue.length > 50) {
+      setCityError("City name must be between 2 and 50 characters!");
+      setIsCityValid(false);
+    } else if (!/^[A-Za-z]+$/.test(cityValue)) {
+      setCityError("Please enter a valid city name!");
+      setIsCityValid(false);
+    } else {
+      setCityError("");
+      setIsCityValid(true);
+    }
+  };
+  
+  const handleCountryChange = (event) => {
+    const countryValue = event.target.value.trim();
+    if (!countryValue) {
+      setCountryError("Please enter country name!");
+      setIsCountryValid(false);
+    } else if (countryValue.length < 2 || countryValue.length > 50) {
+      setCountryError("Country name must be between 2 and 50 characters!");
+      setIsCountryValid(false);
+    } else if (!/^[A-Za-z]+$/.test(countryValue)) {
+      setCountryError("Please enter a valid country name!");
+      setIsCountryValid(false);
+    } else {
+      setCountryError("");
+      setIsCountryValid(true);
+    }
+  };
+
+  const handleGenderChange = (event) => {
+    const genderValue = event.target.value.trim();
+    if (!genderValue) {
+      setGenderError("Please enter gender!");
+      setIsGenderValid(false);
+    } else if (genderValue.length < 1 || genderValue.length > 10) {
+      setGenderError("Gender must be between 1 and 10 characters");
+      setIsGenderValid(false);
+    } else if (!/^[A-Za-z]+$/.test(genderValue)) {
+      setGenderError("Please enter a valid gender!");
+      setIsGenderValid(false);
+    } else if (!/^male$|^female$|^Male$|^Female$/.test(genderValue)) {
+      setGenderError("Please enter a valid gender - Male, Female, male, female!");
+      setIsGenderValid(false);
+    } else {
+      setGenderError("");
+      setIsGenderValid(true);
+    }
+  };
+  
+  const handleAgeChange = (event) => {
+    const ageValue = event.target.value.trim();
+    if (!ageValue) {
+      setAgeError("Please enter a valid age!");
+      setIsAgeValid(false);
+    } else if (!ageValue || isNaN(parseInt(ageValue))) {
+      setAgeError("Age must be a number!");
+      setIsAgeValid(false);
+    } else if (parseInt(ageValue) < 1 || parseInt(ageValue) > 35) {
+      setAgeError("Age must be between 1 and 35");
+      setIsAgeValid(false);
+    } else {
+      setAgeError("");
+      setIsAgeValid(true);
+    }
+  };
+  
+  const handleBreedChange = (event) => {
+    const breedValue = event.target.value.trim();
+    if (!breedValue) {
+      setBreedError("Please enter breed!");
+      setIsBreedValid(false);
+    } else if (breedValue.length < 2 || breedValue.length > 50) {
+      setBreedError("Breed name must be between 2 and 50 characters!");
+      setIsBreedValid(false);
+    } else if (!/^[A-Za-z ]+$/.test(breedValue)) {
+      setBreedError("Please enter a valid breed name!");
+      setIsBreedValid(false);
+    } else {
+      setBreedError("");
+      setIsBreedValid(true);
+    }
+  };
+  
+  // const handleImageChange = (event) => {
+  //   const selectedFile = event.target.files[0];
+  //   if (!selectedFile) {
+  //     setImageError("Please enter an image!");
+  //     setIsImageValid(false);
+  //   } else if (!selectedFile.type.startsWith("image/")) {
+  //     setImageError("Please upload an image!");
+  //     setIsImageValid(false);
+  //   } else {
+  //     setImageError("");
+  //     setIsImageValid(true);
+  //   }
+  // };
+
+  const handleEmailChange = (event) => {
+    const emailValue = event.target.value.trim();
+    if (!emailValue) {
+      setEmailError("Please enter an email!");
+      setIsEmailValid(false);
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue)) {
+      setEmailError("Please enter a valid email!");
+      setIsEmailValid(false);
+    } else {
+      setEmailError("");
+      setIsEmailValid(true);
+    }
+  };
+  
+  
+  const handleAdditionalInfoChange = (event) => {
+    const additionalInfoValue = event.target.value.trim();
+    if(!additionalInfoValue) {
+      setAdditionalInfoError("Please enter additional info!");
+      setIsAdditionalInfoValid(false);
+    } else if (additionalInfoValue.length < 2 || additionalInfoValue.length > 200) {
+      setAdditionalInfoError("Additional info must be less than 200 characters!");
+      setIsAdditionalInfoValid(false);
+    } else {
+      setAdditionalInfoError("");
+      setIsAdditionalInfoValid(true);
+    }
+  };
+
+  useEffect(() => {
+    setFormValid(
+      firstNameError === "" &&
+        lastNameError === "" &&
+        cityError === "" &&
+        countryError === "" &&
+        genderError === "" &&
+        ageError === "" &&
+        breedError === "" &&
+        imageError === "" &&
+        additionalInfoError === "" &&
+        emailError === "" &&
+        passwordError === ""
+    );
+  }, [   passwordError, firstNameError,    lastNameError,    cityError,    countryError,    genderError,    ageError,    breedError,    imageError,    additionalInfoError,    emailError,  ]);
+  
+
+  // const checkFormValidity = () => {
+  //   if (!isFirstNameValid || !isLastNameValid || !isCityValid || !isCountryValid || !isGenderValid || !isAgeValid || !isImageValid || !isEmailValid || !isPasswordValid || !isAdditionalInfoValid) {
+  //       setFormValid(false);
+  //   } else {
+  //     setFormValid(true);
   //   }
   // };
 
@@ -153,8 +385,8 @@ export const Register = ({ setCards }) => {
       setAgeError("Please enter a valid age!");
     } else if (!age || isNaN(parseInt(age))) {
       setAgeError("Age must be a number!");
-    } else if (parseInt(age) < 5 || parseInt(age) > 110) {
-      setAgeError("Age must be between 5 and 110");
+    } else if (parseInt(age) < 1 || parseInt(age) > 35) {
+      setAgeError("Age must be between 1 and 35");
     } else {
       setAgeError("");
     }
@@ -283,7 +515,9 @@ export const Register = ({ setCards }) => {
       );
     } catch (err) {
       // setErr(true);
-      setEmailError("This email is already in use");
+      if (email) {
+        setEmailError("This email is already in use");
+      }
     }
   };
 
@@ -323,6 +557,9 @@ export const Register = ({ setCards }) => {
                   autoFocus
                   helperText={firstNameError}
                   error={Boolean(firstNameError)}
+                  inputProps={{
+                    onChange: handleFirstNameChange,
+                  }}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -335,6 +572,9 @@ export const Register = ({ setCards }) => {
                   autoComplete="family-name"
                   helperText={lastNameError}
                   error={Boolean(lastNameError)}
+                  inputProps={{
+                    onChange: handleLastNameChange,
+                  }}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -348,6 +588,9 @@ export const Register = ({ setCards }) => {
                   autoFocus
                   helperText={cityError}
                   error={Boolean(cityError)}
+                  inputProps={{
+                    onChange: handleCityChange,
+                  }}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -360,6 +603,9 @@ export const Register = ({ setCards }) => {
                   autoComplete="country"
                   helperText={countryError}
                   error={Boolean(countryError)}
+                  inputProps={{
+                    onChange: handleCountryChange,
+                  }}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -372,6 +618,10 @@ export const Register = ({ setCards }) => {
                   autoComplete="gender"
                   helperText={genderError}
                   error={Boolean(genderError)}
+                  handleGenderChange
+                  inputProps={{
+                    onChange: handleGenderChange,
+                  }}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -384,6 +634,10 @@ export const Register = ({ setCards }) => {
                   autoComplete="age"
                   helperText={ageError}
                   error={Boolean(ageError)}
+                  handleAgeChange
+                  inputProps={{
+                    onChange: handleAgeChange,
+                  }}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -396,6 +650,9 @@ export const Register = ({ setCards }) => {
                   autoComplete="breed"
                   helperText={breedError}
                   error={Boolean(breedError)}
+                  inputProps={{
+                    onChange: handleBreedChange,
+                  }}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -411,6 +668,9 @@ export const Register = ({ setCards }) => {
                   }}
                   helperText={imageError}
                   error={Boolean(imageError)}
+                  // inputProps={{
+                  //   onChange: handleImageChange,
+                  // }}
                 />
                 <Button variant="contained" component="label">
                   Upload Image
@@ -444,6 +704,9 @@ export const Register = ({ setCards }) => {
                   autoComplete="email"
                   helperText={emailError}
                   error={Boolean(emailError)}
+                  inputProps={{
+                    onChange: handleEmailChange,
+                  }}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -457,6 +720,10 @@ export const Register = ({ setCards }) => {
                   autoComplete="new-password"
                   helperText={passwordError}
                   error={Boolean(passwordError)}
+                  inputProps={{
+                    onChange: handlePasswordChange,
+                  }}
+                  
                 />
               </Grid>
               <Grid item xs={12}>
@@ -471,6 +738,9 @@ export const Register = ({ setCards }) => {
                   rows={4}
                   helperText={additionalInfoError}
                   error={Boolean(additionalInfoError)}
+                  inputProps={{
+                    onChange: handleAdditionalInfoChange,
+                  }}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -487,6 +757,8 @@ export const Register = ({ setCards }) => {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              disabled={!formValid}
+              // onClick={checkFormValidity}
             >
               Register
             </Button>
